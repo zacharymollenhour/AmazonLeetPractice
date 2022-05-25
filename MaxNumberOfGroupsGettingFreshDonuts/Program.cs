fe31631f18289dbc0156d19a34b237882d3d43a6
Zachary <zachary.mollenhour@plena.io>
2022-05-25 09:14:18 -0500

    updates

---
+++ MaxNumberOfGroupsGettingFreshDonuts/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxNumberOfGroupsGettingFreshDonuts
+{
+    /// <summary>
+    /// There is a donuts shop that bakes donuts in batches of (batchSize). They have a rule where they must server all of the donuts of a batch before serving any of the next batch
+    /// You are given an int (batchSize) and an int array (groups) where (groups[i]) denotes that there is a group of groups[i] customers that will visit the shop. Each customer will get exactly one donut
+    /// When a group visits the shop, all customers of the group must be served before serving any of the following groups. 
+    /// A group will be happy if they all get fresh donuts. That is, the first customer of the group does not recieve a donut that was left over from the previous group
+    /// You can freely rearrange the ordering of the groups
+    /// Return the max possibly number of happy groups after rearranging the groups
+    /// Example:
+    ///     Input: batchSize = 3, groups = [1,2,3,4,5,6]
+    ///     Output: 4
+    ///     Explanation: You can arrange the groups as [6,2,4,5,1,3]. Then the 1st, 2nd, 4th, and 6th groups will be happy
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int batchSize = 3;
+            int[] groups = new int[] { 1, 2, 3, 4, 5, 6 };
+
+
+        }
+    }
+}
---
+++ RussianDollEnvelopes/Program.cs
@@ -0,0 +1,58 @@
+﻿using java.util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RussianDollEnvelopes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int result = maxEnvelopes(new int[][] {[[5,4], [6,4], [6,7], [2,3] } )
+        }
+
+        public static int maxEnvelopes(int[][] envelopes)
+
+        {
+            if (envelopes.Length == 0)
+                return 0;
+
+            if (envelopes.Length == 1)
+                return 1;
+
+            Array.Sort(envelopes, (e1, e2) =>
+            {
+                var compare = e1[0].CompareTo(e2[0]);
+                if (compare != 0)
+                    return compare;
+                return e1[1].CompareTo(e2[1]);
+            });
+
+            int[] dp = new int[envelopes.Length];
+
+            dp[0] = 1;
+
+            for (int i = 1; i < envelopes.Length; i++)
+            {
+                dp[i] = 1;
+                var curr = envelopes[i];
+
+                for (int j = 0; j < i; j++)
+                {
+                    var prevDP = dp[j];
+                    var prev = envelopes[j];
+
+                    if (prev[0] < curr[0] && prev[1] < curr[1])
+                    {
+                        if (prevDP + 1 > dp[i])
+                            dp[i] = prevDP + 1;
+                    }
+                }
+            }
+            return dp.Max();
+        }
+    }
+}
