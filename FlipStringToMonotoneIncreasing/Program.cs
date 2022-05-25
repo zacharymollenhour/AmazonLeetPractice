14bab1162f69bd8a6f71e0281d9b1eedbd571f38
Zachary <zachary.mollenhour@plena.io>
2022-05-25 05:08:42 -0500

    FlipStringToMonotoneIncreasing

---
+++ FlipStringToMonotoneIncreasing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipStringToMonotoneIncreasing
+{
+    /// <summary>
+    /// A binary string is monotone increasing if it consists of some number of 0's (possibly none) followed by some number of 1's (also possibly none)
+    /// You are given a binary string (s). You can flip s[i] changing it from 0 to 1 or from 1 to 0
+    /// Return the min number of flips to make s monotone increasing
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int s = 00110;
+            bool numFlips = NumberOfFlipsToBeMonotoneIncreasing(s);
+            Console.WriteLine(numFlips);
+            Console.ReadKey();
+        }
+
+        private static bool NumberOfFlipsToBeMonotoneIncreasing(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+
+            int revertedNumber = 0;
+            while (x > revertedNumber)
+            {
+                revertedNumber = revertedNumber * 10 + x % 10;
+                x /= 10;
+            }
+
+            return x == revertedNumber || x == revertedNumber / 10;
+        }
+    }
+}
--- ReverseInteger/Program.cs
+++ ReverseInteger/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace ReverseInteger
 {
+    /// <summary>
+    /// Given a signed 32-bit integer (x), return (x) with its digits reversed. If reversing x causes to value to go outside the signed 32-bit int range, then return 0
+    /// Example: 
+    ///     Input: X = 123
+    ///     Output: 321
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
         {
+            Int32 x = 123;
+            Console.WriteLine(Reverse(x));
+            Console.ReadKey();
+        }
+
+        private static Int32 Reverse(Int32 x)
+        {
+            
+            char[] splitInt = x.ToCharArray();
+            Array.Reverse(splitInt);
+            string splitIntString = splitInt.ToString();
+            Console.WriteLine(splitIntString);
+            return Convert.ToInt32(splitIntString);
         }
     }
 }
