b2fec4d476d158ba566e48d743fddaa2b1226213
Zachary <zachary.mollenhour@plena.io>
2022-05-25 01:54:34 -0500

    LongestPalindromeSubstring

--- AddTwoNumbersInLinkedList/Program.cs
+++ AddTwoNumbersInLinkedList/Program.cs
@@ -6,27 +6,75 @@ using System.Threading.Tasks;
 
 namespace AddTwoNumbersInLinkedList
 {
+    /// <summary>
+    /// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverese order, and each of their nodes
+    /// contains a single digit. Add the two numbers nad return the sum as  alinked list
+    /// </summary>
+
+    /// Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            int[] l1 = new int[] { 2, 4, 3 };
-            int[] l2 = new int[] { 5, 6, 4 };
 
-            int[] result = new int[l1.Count()];
-            int[] reversedL1 = new int[l1.Count()];
-
-            int length = l1.Length - 1;
-            Console.WriteLine($"Length Of List1: {length}");
-            for (int i = length; i >= 0; i--)
-            {
-                //
-                // reversedL1 = reversedL1;
-            }
-
-            Console.WriteLine($"L1 result: {l1.ToString()}");
-            Console.ReadKey();
         }
 
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            // Result Head
+            ListNode start = null;
+            ListNode head = null;
+
+            var remainder = 0;
+            // Keep on adding the values until there are no nodes left
+            while (l1 != null || l2 != null || remainder > 0)
+            {
+                // Insert a value of 0 for any that are non existent
+                var n1 = l1 != null ? l1.val : 0;
+                var n2 = l2 != null ? l2.val : 0;
+
+                // Now add the node values together
+                var sumOfNodes = n1 + n2 + remainder;
+
+                // Set remainder to 0 again
+                remainder = 0;
+
+                // Check if the sum >= 10, as the remainder cannot be more than 1 (max sum == 9 + 9 + 1 == 19)
+                if (sumOfNodes >= 10)
+                {
+                    remainder = 1;
+                }
+
+                // Dont exculde the last node
+                var prev = head;
+
+                // Create a new node
+                head = new ListNode(sumOfNodes % 10);
+
+                // Connect the last node to the current head
+                if (prev != null)
+                {
+                    prev.next = head;
+                }
+                // If there was no previous node, then this node will be the start
+                else
+                    start = head;
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+            return start;
+        }
     }
 }
---
+++ LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestPalindromicSubstring
+{
+    /// <summary>
+    /// Given a string s, return the longest palindromic substring in s
+    /// Example:
+    ///     Input: s = "badab"
+    ///     Output: "bab"
+    ///     Explanation: "aba" is also a valid answer
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = "babad";
+            Console.WriteLine($"For string 'babad' : result: {LongestPalindrome(s)}");
+
+            s = "cbbd";
+            Console.WriteLine($"For string 'cbbd' : result: {LongestPalindrome(s)}");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Function that will split out the string into a current, left, odd and max node int variable
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string LongestPalindrome(string s)
+        {
+            if (s.Length <= 1) return s;
+
+            int left = 0, odd, even, curr, max = 0;
+
+            //Loop through each value in the string
+            for (int i = 0; i < s.Length; i++)
+            {
+                // First, we can check for a palindrom of the odd length
+                // We will pass in the s value and the current iteration twice
+                // Example: 121
+                odd = GetPalindrome(s, i, i);
+
+                // Second, wew can check for a palindrom off the even length
+                // We will pass in the s value, the current iterator and the next iteration
+                // Example 1221
+                even = GetPalindrome(s, i, i + 1);
+
+                // Now we want to compare the two results
+                curr = (odd > even ? odd : even);
+
+                // If the new current value is greater than the last max, update max
+                if (curr > max)
+                {
+                    // Set the new max value
+                    max = curr;
+
+                    //calculate new left value
+                    left = i - (max - 1) / 2;
+
+                }
+            }
+
+            return s.Substring(left, max);
+        }
+
+        private static int GetPalindrome(string s, int left, int right)
+        {
+            // While the left value is equal to the first run or later,
+            // the right values still exist,
+            // and the left nad right values in string are equal
+            // We continue traversing, as this is a palindome
+            while(left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+
+            return right - left - 1;
+        }
+    }
+}
